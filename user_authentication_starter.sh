9b31017a21acf18291c2ae28a9cb7f324f1756b4
Peter Ahumuza <peter@172-29-104-133.w.africa.local.cmu.edu>
2023-08-08 14:48:50 +0200

    Changes to be committed: 	modified:   data/credentials.txt 	deleted:    data/log.logged_in 	modified:   user_authentication_starter.sh

--- user_authentication_starter.sh
+++ user_authentication_starter.sh
@@ -131,29 +131,23 @@ login() {
     done <$credentials_file
 }
 
-logged_in_menu() {
-    echo "You are logged in as ${username}"
-    echo "1. l"
-    echo "2. Create account"
-    echo "3. exit"
-    echo
-    read -p "Select an option [1-3]: " option
-}
 
-logout() {
-    user=$1
+log_out() {
+    user=$(cat "$logged_in_file")
+    echo $user
     #TODO: check that the .logged_in file is not empty
     # if the file exists and is not empty, read its content to retrieve the username
     # of the currently logged in user
 
     # then delete the existing .logged_in file and update the credentials file by changing the last field to 0
 
-    while IFS=: read -r username hash salt role logged; do
+    while IFS=: read -r username hash salt fullname role logged; do
         if [[ $user = $username ]]; then
-            search_string="$username:$hash:$salt:$role:1+"
-            replace_string="$username:$hash:$salt:$role:0"
+            search_string="$username:$hash:$salt:$fullname:$role:$logged"
+            replace_string="$username:$hash:$salt:$fullname:$role:0"
 
             sed -i "s/$search_string/$replace_string/" $credentials_file
+            rm $logged_in_file
         fi
     done <$credentials_file
     return 0
@@ -171,13 +165,36 @@ menu() {
     read -p "Select an option [1-3]: " option
 }
 
+logged_in_menu() {
+    echo "You are logged in as ${username}"
+
+    if [[ $role = 'admin' ]]
+    then
+        echo "2. Create account"
+    fi
+
+    echo "3. exit"
+    echo "4. logout"
+    echo
+    read -p "Select an option: " option
+}
+
 # After the user is logged in, display a menu for logging out.
 # if the user is also an admin, add an option to create an account using the
 # provided functions.
 
 # Main script execution starts here
 #echo "Welcome to the authentication system."
-menu
+get_menu() {
+    if [[ -f "$logged_in_file" ]]
+    then 
+        logged_in_menu
+    else
+        menu
+    fi
+}
+
+get_menu
 
 while [[ true ]]; do
     if [[ $option -eq 1 ]]; then
@@ -188,17 +205,21 @@ while [[ true ]]; do
         read -p "Enter your fullname: " fullname
         read -rs -p "Enter your password: " password
         echo
-        register_credentials "${username}" "${password}" "${fullname}"
+        read -p "Enter role (admin , salesperson, normal - default normal): " role
+
+        register_credentials "${username}" "${password}" "${fullname}" "${role}"
     elif [[ $option -eq 3 ]]; then
         echo
         echo "Good-bye...!"
         exit 0
+    elif [[ $option -eq 4 ]]; then
+        log_out
     else
         clear
         echo "Invalid choice ${option} selected - try again"
     fi
 
-    menu
+    get_menu
 done
 #### BONUS
 #1. Implement a function to delete an account from the file
